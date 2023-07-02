2748f8a4df7bfb7f6d8faa83ad9473a45713620f
Darren Henderson <darrenh@ccsj.edu>
2023-07-01 19:56:20 -0500

    First commit. Have working Laravel 10/Inertia/Vue 3 Highcharts Sankey example.

---
+++ resources/js/Pages/Charts/Examples.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Sankey from "@/Components/Charts/Sankey.vue";
+</script>
+
+<template>
+
+    <div>HighCharts Examples</div>
+    <p>Hey Dude!</p>
+
+    <Sankey></Sankey>
+</template>
